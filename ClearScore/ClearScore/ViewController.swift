9b621d882aa7a16ce0b862f05784e9b639236474
Jack.LiuKui <jack.liukui@takealot.com>
2021-02-05 16:08:05 +0200

    inital project

---
+++ ClearScore/ClearScore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClearScore
+//
+//  Created by kingjack on 2021/02/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
